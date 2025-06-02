2d46a7e019f621b52f02d1992b187269b92f9c98
Willian Menezes da Silva <willmenezes@outlook.com.br>
2025-06-01 23:48:45 -0300

    feat: atualiza 01 - Listas - 00 declarando listas

--- 00 - Fundamentos/string_2.py
+++ 00 - Fundamentos/string_2.py
@@ -6,17 +6,17 @@ saldo = 45.435
 
 dados = {"nome": "Guilherme", "idade": 28}
 
-print("Nome: %s Idade: %d" % (nome, idade))
+print("Nome: %s Idade: %d" % (nome, idade)) # Formatação antiga
 
-print("Nome: {} Idade: {}".format(nome, idade))
+print("Nome: {} Idade: {}".format(nome, idade)) # Formatação com .format()
 
-print("Nome: {1} Idade: {0}".format(idade, nome))
-print("Nome: {1} Idade: {0} Nome: {1} {1}".format(idade, nome))
+print("Nome: {1} Idade: {0}".format(idade, nome)) # Formatação com .format() e índices
+print("Nome: {1} Idade: {0} Nome: {1} {1}".format(idade, nome)) # Formatação com .format() e índices repetidos
 
 print("Nome: {nome} Idade: {idade}".format(nome=nome, idade=idade))
 print("Nome: {name} Idade: {age} {name} {name} {age}".format(age=idade, name=nome))
 print("Nome: {nome} Idade: {idade}".format(**dados))
 
-print(f"Nome: {nome} Idade: {idade}")
-print(f"Nome: {nome} Idade: {idade} Saldo: {saldo:.2f}")
-print(f"Nome: {nome} Idade: {idade} Saldo: {saldo:10.1f}")
+print(f"Nome: {nome} Idade: {idade}") # Formatação com f-strings (Python 3.6+)
+print(f"Nome: {nome} Idade: {idade} Saldo: {saldo:.2f}") # Formatação com f-strings e formatação de float
+print(f"Nome: {nome} Idade: {idade} Saldo: {saldo:10.1f}") # Formatação com f-strings e formatação de float com largura mínima
--- 01 - Estrutura de dados/01 - Listas/00_declarando_listas.py
+++ 01 - Estrutura de dados/01 - Listas/00_declarando_listas.py
@@ -7,8 +7,9 @@ print(frutas)
 letras = list("python")
 print(letras)
 
-numeros = list(range(10))
+numeros = list(range(10)) # ou: numeros = list(range(0, 10))
 print(numeros)
 
+# Em python, listas podem conter diferentes tipos de dados:
 carro = ["Ferrari", "F8", 4200000, 2020, 2900, "São Paulo", True]
 print(carro)
