f6be9c5034a3c5fd6cf1a898c0a4be5df336165d
Willian Menezes da Silva <willmenezes@outlook.com.br>
2025-06-02 10:14:08 -0300

    atualiza 01 - estrutura de dados - Listas

--- 01 - Estrutura de dados/01 - Listas/13_pop.py
+++ 01 - Estrutura de dados/01 - Listas/13_pop.py
@@ -1,6 +1,11 @@
 linguagens = ["python", "js", "c", "java", "csharp"]
 
+# pop remove o último elemento da lista e o retorna
 print(linguagens.pop())  # csharp
+print(linguagens)  # ["python", "js", "c", "java"]
 print(linguagens.pop())  # java
+print(linguagens)  # ["python", "js", "c"]
 print(linguagens.pop())  # c
+print(linguagens)  # ["python", "js"]
 print(linguagens.pop(0))  # python
+print(linguagens)  # ["js"]
