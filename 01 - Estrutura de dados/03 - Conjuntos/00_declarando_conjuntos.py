43ca9043024e93fd49464a1646ccc02c6528a811
Willian Menezes da Silva <willmenezes@outlook.com.br>
2025-06-04 11:59:02 -0300

    atualiza 01 - Estruturas de dados - Tuplas

--- 01 - Estrutura de dados/03 - Conjuntos/00_declarando_conjuntos.py
+++ 01 - Estrutura de dados/03 - Conjuntos/00_declarando_conjuntos.py
@@ -1,3 +1,6 @@
+# Conjuntos são coleções de elementos únicos e não ordenados.
+# Eles são úteis quando você precisa garantir que não haja duplicatas em uma coleção de itens.
+
 numeros = set([1, 2, 3, 1, 3, 4])
 print(numeros)  # {1, 2, 3, 4}
 
--- 01 - Estrutura de dados/03 - Conjuntos/01_acessando_dados.py
+++ 01 - Estrutura de dados/03 - Conjuntos/01_acessando_dados.py
@@ -1,5 +1,7 @@
 numeros = {1, 2, 3, 2}
 
+# Conjuntos não suportam indexação, mas você pode convertê-los em uma lista se precisar acessar 
+# elementos por índice.
 numeros = list(numeros)
-
+print(numeros)
 print(numeros[0])
