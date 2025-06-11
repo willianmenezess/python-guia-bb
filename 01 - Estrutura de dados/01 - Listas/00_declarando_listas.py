22048d0f0c167071615dc03450550a849e947c1b
Willian Menezes da Silva <willmenezes@outlook.com.br>
2025-06-11 12:22:22 -0300

    comentários em Listas

--- 01 - Estrutura de dados/01 - Listas/00_declarando_listas.py
+++ 01 - Estrutura de dados/01 - Listas/00_declarando_listas.py
@@ -1,3 +1,9 @@
+# Os itens da lista são ordenados, alteráveis e permitem valores duplicados. Os itens da lista são 
+# indexados, o primeiro item possui índice [0], o segundo item possui índice [1], etc. Quando dizemos 
+# que as listas estão ordenadas, significa que os itens têm uma ordem definida, e essa ordem não 
+# será alterada. Se você adicionar novos itens a uma lista, os novos itens serão colocados 
+# no final da lista.
+
 frutas = ["laranja", "maca", "uva"]
 print(frutas)
 
